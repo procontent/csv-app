9da5bceecc1ff9ac1bf8ba97df9d572b1e38d4ca
Curtis Fisher <cfisher@roi-selling.com>
2025-03-09 11:39:58 -0700

    6oInitialized Repo

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            external: ['react', 'react-dom', 'react/jsx-runtime']
+        }
+    },
+    resolve: {
+        alias: {
+            react: 'react',
+            'react/jsx-runtime': 'react/jsx-runtime'
+        }
+    },
+    server: {
+        port: 5173,
+    },
+});
